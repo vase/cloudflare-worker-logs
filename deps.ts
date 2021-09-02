08d84542054934a2d82ccf047f2c085ed29fb380
Asyrique <asyrique@gmail.com>
2021-09-03 00:40:51 +0800

    further move to 23

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { MongoClient } from "https://deno.land/x/mongo@v0.24.0/mod.ts";
+export { MongoClient } from "https://deno.land/x/mongo@v0.23.1/mod.ts";
 export * as Colours from "https://deno.land/std@0.106.0/fmt/colors.ts"
